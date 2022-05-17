12e5ed2cc0254703993557a0528114665a7d0a7f
Adithya Satyanarayana <adithya.satyanarayana@yahoo.com>
2022-05-17 14:30:36 +0530

    Add styling css code.
    How the data should be displayed, or how the page should appear is
    styled inside meetup-details and all-meetups css files. These files are
    referenced/linked inside the main index.html and the appropriate code to
    display data is written.
    Data format coming in from views has been updated/modified.

--- revisit_django/meetups/views.py
+++ revisit_django/meetups/views.py
@@ -6,7 +6,27 @@ from django.shortcuts import render
 def index(request):
     # return HttpResponse("This will be displayed on the landing page.")
 
+    # Template's output. The data needs to be injected into the template.
+    meetups = [
+        {
+            'title': 'First meetup',
+            'location': 'Basavangudi',
+            'genre': 'cultural',
+            'slug': 'cult-meetup'
+        },
+        {
+            'title': 'Second meetup',
+            'location': 'Jayanagar',
+            'genre': 'sports',
+            'slug': 'sport-meetup'
+        }
+    ]
+
     # Render will create a HTTP response behind the scene, to return a rendered tempalate.
     # The path to the template is relative from the template folder.
     # Not just read the HTML content, but will parse, find and execute special instruction and return the final processed content.
-    return render(request, 'meetups/index.html')
+    # Third argument (dictionary) : Data which has to be accessible inside the template for output.
+    return render(request, 'meetups/index.html', {
+        'show_meetups': True,
+        'meetups': meetups
+    })
