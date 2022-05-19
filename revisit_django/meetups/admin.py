b874e231f1c5584dd7d3deac2451ba7cf70eef15
Adithya Satyanarayana <adithya.satyanarayana@yahoo.com>
2022-05-19 12:31:47 +0530

    Add location and customize admin console

--- revisit_django/meetups/admin.py
+++ revisit_django/meetups/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from .models import Meetup
+from .models import Location, Meetup
 
 # Register your models here.
-admin.site.register(Meetup)
+
+class MeetupAdmin(admin.ModelAdmin):
+    # Case sensitive to the field which is already defined.
+    list_display = ('title', 'slug')
+    list_filter = ('location',)
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location)
--- revisit_django/meetups/models.py
+++ revisit_django/meetups/models.py
@@ -1,10 +1,22 @@
 from distutils.command.upload import upload
+from operator import mod
 from django.db import models
 
 # Create your models here.
 
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=350)
+
+    def __str__(self):
+        return f'{self.name} - {self.address}'
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)        # Unique as True will act similar to a primary key. But not a PK.
-    description = models.TextField()            # TextField is used to store larger data than CharField
-    image = models.ImageField(upload_to='images')                 # Stores a reference to the path of the file. Does not actually store the image.
+    slug = models.SlugField(unique=True)            # Unique as True will act similar to a primary key. But not a PK.
+    description = models.TextField()                # TextField is used to store larger data than CharField
+    image = models.ImageField(upload_to='images')   # Stores a reference to the path of the file. Does not actually store the image.
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} : {self.slug}'
